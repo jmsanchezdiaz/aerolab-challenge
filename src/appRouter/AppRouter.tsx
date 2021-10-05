197a77ec4ed41c24a36435d80dc2545b3ae14efd
Juan Manuel Sanchez Diaz <jmsanchezdiaz02@gmail.com>
2021-10-05 18:19:20 -0300

    router-changes

--- src/appRouter/AppRouter.tsx
+++ src/appRouter/AppRouter.tsx
@@ -1,10 +1,22 @@
-import { Route, BrowserRouter as Router } from "react-router-dom";
-import PublicRoutes from "./PublicRoutes";
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import NavBar from "../components/NavBar/NavBar";
+import ProductPage from "../pages/ProductsPage/ProductPage";
+import ProfilePage from "../pages/ProfilePage/ProfilePage";
 
 const AppRouter = () => {
   return (
     <Router>
-      <Route path="/aerolabchallenge" component={PublicRoutes} />
+      <NavBar />
+      <Switch>
+        <Route exact path="/profile" component={ProfilePage} />
+        <Route exact path="/" component={ProductPage} />
+        <Redirect to="/" />
+      </Switch>
     </Router>
   );
 };
